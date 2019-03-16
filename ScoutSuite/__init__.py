b9a268a5a0bee6e4ccf1ff3dfd273407822930c9
Vincent Fortin <vincent-2.fortin@polymtl.ca>
2019-03-16 13:13:17 -0400

    Renamed AWS functions to be more general

--- ScoutSuite/__init__.py
+++ ScoutSuite/__init__.py
@@ -4,11 +4,10 @@ __author__ = 'NCC Group'
 __version__ = '5.0.0-beta'
 
 DEFAULT_REPORT_DIR = 'scoutsuite-report'
-DEFAULT_RESULT_FILE = 'scoutsuite-results/scoutsuite_results.js'
-DEFAULT_RULESET_FILE = 'scoutsuite-results/scoutsuite_ruleset.js'
-DEFAULT_EXCEPTIONS_FILE = 'scoutsuite-results/scoutsuite_exceptions.js'
-DEFAULT_ERRORS_FILE = 'scoutsuite-results/scoutsuite_errors_log.json'
-DEFAULT_HTMLREPORT_FILE = 'report.html'
+AWSCONFIG_FILE = 'scoutsuite-results/scoutsuite_results.js'
+AWSRULESET_FILE = 'scoutsuite-results/scoutsuite_ruleset.js'
+EXCEPTIONS_FILE = 'scoutsuite-results/scoutsuite_exceptions.js'
+HTMLREPORT_FILE = 'report.html'
 
 ERRORS_LIST = []
 
